6121f3b5dbd808a78622171e0ef0f5d226b65a14
Alex Drozhak <alex.drozhak@me.com>
2016-09-28 18:19:54 +0300

    Adds shared framework build scheme for Carthage

---
+++ JSQMessagesFramework/JSQMessagesFramework/JSQMessagesFramework.h
@@ -0,0 +1,21 @@
+//
+//  JSQMessagesFramework.h
+//  JSQMessagesFramework
+//
+//  Created by Alex Drozhak on 9/28/16.
+//  Copyright © 2016 Alex Drozhak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSQMessagesFramework.
+FOUNDATION_EXPORT double JSQMessagesFrameworkVersionNumber;
+
+//! Project version string for JSQMessagesFramework.
+FOUNDATION_EXPORT const unsigned char JSQMessagesFrameworkVersionString[];
+
+#import <JSMessagesFramework/JSQMessages.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesFramework/PublicHeader.h>
+
+
