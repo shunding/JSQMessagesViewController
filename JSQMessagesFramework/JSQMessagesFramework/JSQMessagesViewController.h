15b55d9613dfbd39966c1885baa58dcb88863783
Alex Drozhak <alex.drozhak@me.com>
2016-09-29 12:17:36 +0300

    Fixes type in header declaration

--- JSQMessagesFramework/JSQMessagesFramework/JSQMessagesViewController.h
+++ JSQMessagesFramework/JSQMessagesFramework/JSQMessagesViewController.h
@@ -14,7 +14,7 @@ FOUNDATION_EXPORT double JSQMessagesFrameworkVersionNumber;
 //! Project version string for JSQMessagesFramework.
 FOUNDATION_EXPORT const unsigned char JSQMessagesFrameworkVersionString[];
 
-#import <JSMessagesFramework/JSQMessages.h>
+#import <JSQMessagesFramework/JSQMessages.h>
 
 // In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesFramework/PublicHeader.h>
 
