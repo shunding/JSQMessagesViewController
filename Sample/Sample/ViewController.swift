25454880fe12e07c434b335d34f4b73603bedcfe
Alex Drozhak <alex.drozhak@me.com>
2016-09-30 21:36:45 +0300

    Carthage support

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Alex Drozhak on 9/29/16.
+//  Copyright © 2016 Alex Drozhak. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
